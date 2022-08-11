3a695eda2f8fdaec8df92abe08e1c13fe396be26
shahraiz <shahraizali10@yahoo.com>
2022-08-11 22:45:07 +0200

    bulk

--- bulk-generation.sh
+++ bulk-generation.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 # declare the name of modules here
-array=( "name" "your" "modules" )
-for i in "${array[@]}"
+yourfilenames=`ls ./*.png`
+for i in "$yourfilenames"
 do
-	bash cookiecutter https://github.com/shahraizali/cb-screen-module-sample project_name=$i --no-input
+	# strip the extension
+	filename=$(basename "$i" .png)
+	# add space in between the words
+	filename=$(echo "$filename" | sed 's/_/ /g')
+	bash cookiecutter https://github.com/shahraizali/cb-screen-module-sample project_name=$filename --no-input
 done
